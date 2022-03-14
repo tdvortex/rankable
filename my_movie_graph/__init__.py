fb9ea6887fbc1ea05d5550d3d38831afc5b3bf8d
tdvortex <tiedyedvortex@gmail.com>
2022-03-14 18:00:11 -0500

    Clean up __init__.py slightly

--- my_movie_graph/__init__.py
+++ my_movie_graph/__init__.py
@@ -4,15 +4,13 @@ from flask import Flask
 def create_app():
     """Creates and configures main driver for my_movie_graph"""
     app = Flask(__name__, instance_relative_config=True)
+    app.secret_key = os.environ.get("SECRET_KEY") or os.urandom(24)
 
     # Confirm instance folder exists
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
-
-    # Associate path 'index' with path '/'
-    #app.add_url_rule('/', endpoint='index')
 
     # Temporary Hello World page
     from . import hello
