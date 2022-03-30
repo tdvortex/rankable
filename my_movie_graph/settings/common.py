4d227ce2c09e4d4af241ca8256e957a5f0e71c86
tdvortex <tiedyedvortex@gmail.com>
2022-03-30 17:00:14 -0500

    Isolate dev config settings

--- my_movie_graph/settings/common.py
+++ my_movie_graph/settings/common.py
@@ -15,19 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)6uuj=j=0*ph0gq^1mrgt*y@a4&4li@heb8%=h7jcw1(hgtr5w'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
 # Application definition
 
 INSTALLED_APPS = [
